60bbf0b36206da8df54e6e090af9063434a0829a
octesian <octesian@protonmail.com>
2016-10-22 10:45:10 -0700

    wrote sensor-neutral code to handle 3-character commands from the base station.

--- SerialPortCommArduino/SerialPortCommArduino.ino
+++ SerialPortCommArduino/SerialPortCommArduino.ino
@@ -1,48 +1,56 @@
-#include <Servo.h>
+#include <MsTimer2.h>
+#include <Streaming.h>
 
-Servo myservo;
-int angle = 0;
-int newAngle = 0;
-const int MaxChars = 4;
-char strValue[MaxChars+1];
-int index = 0;
+#define DELAY 100
+#define SLOW 500
+#define FAST 10
 
-//void serialEvent()
-//{
-//   while(Serial.available()) 
-//   {
-//      char ch = Serial.read();
-//      Serial.write(ch);
-//      if(index < MaxChars && isDigit(ch)){
-//          strValue[index++] = ch;              
-//          }else{
-//            strValue[index] = 0;                    
-//            newAngle = atoi(strValue);                    
-//            if(newAngle > 0 && newAngle <180){
-//            if(newAngle < angle)                                
-//              for(; angle > newAngle; angle -= 1) {
-//                   myservo.write(angle);
-//               }  
-//            else 
-//               for(; angle < newAngle; angle += 1){
-//                   myservo.write(angle);
-//                } 
-//         }
-//         index = 0;
-//         angle = newAngle;
-//      }  
-//   }
-//}
+/* valid 'sensor' is a character '1' or '2' */
+char sensor = '0';
 
-
-void setup()
-{
+void setup(){
   Serial.begin(57600);
-  myservo.attach(10);
-  angle = 90;  
 }
 
-void loop()
-{
-    Serial.println("Band Camp");
+void readSensor(){
+  if( sensor == '1' ){
+    //Read from first sensor
+    //Serial.println(analogRead(0), DEC);
+    Serial.println("Reading from sensor '1'");
+  }else if( sensor == '2' ){
+    //Read from second sensor
+    Serial.println("Reading from sensor '2'");
+  }
+}
+
+void loop(){
+  char cmd = '0';
+  char rateChar = '0';
+  int rate = 0;
+
+    cmd = Serial.read();
+    if( cmd  == 'R' ){
+      delay(DELAY);
+      sensor = Serial.read();
+      delay(DELAY);
+      rateChar = Serial.read();
+      if( (rateChar == 'S' || rateChar == 'F') &&
+          (sensor == '1' || sensor == '2') ){
+        if(rateChar == 'S'){
+          rate = SLOW;
+        }else if( rateChar == 'F' ){
+          rate = FAST;
+        }
+        //start reading;
+        MsTimer2::stop();
+        MsTimer2::set( rate, readSensor );
+        MsTimer2::start();
+      }
+      
+    }else if( cmd == 'S' ){
+      //stop reading
+      MsTimer2::stop();
+    }
+
+    delay(DELAY);
 }
