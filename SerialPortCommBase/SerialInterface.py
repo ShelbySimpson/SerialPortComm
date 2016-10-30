c9543a846a6fd7ee421a3376ba25ca05101005b8
SS-simpson <shelbywsimpson@gmail.com>
2016-10-30 09:07:23 -0700

    fixed buffer issues, also now prints data to time stamped csv file, completed

--- SerialPortCommBase/SerialInterface.py
+++ SerialPortCommBase/SerialInterface.py
@@ -5,9 +5,7 @@ import sys
 from time import sleep
 import os
 import signal
-import csv
 import string
-from msvcrt import getch
 
 #explain command line inputs
 def usage():
@@ -23,21 +21,26 @@ baud = 56700;
 
 #clean up tasks before program exits
 def signal_handler(signal, frame):
-    #while ser.inWaiting():
-        #strData = str(ser.read(5),'utf-8');#convert bytes
-        #strData = strData.rstrip();#strip newlines
-        #data.append(strData);#add to list
-        #data.append("\r");#newline
-        #sys.stdout.flush()
-        #print(strData);
+    #empty out whats left in serial and save to data list
+    while ser.inWaiting(): 
+        sData = ser.readline().strip();
+        data.append(sData);
+        sys.stdout.flush()
+
     #create a timestamp name for file
     sensorDataName = time.strftime("%m_%d_%y_%H_%M",time.localtime());
+    #add rate sensor details to filename
     sensorDataName = sensorDataName + "_" + sensor + "-" + rate + ".csv";
     #write data list to file,close file
     sensorDataFile = open(sensorDataName,'w');
+    #write data to file
+    print("Writing data to file...");
     for item in data:
-        sensorDataFile.write(item);
+        item = str(item,'utf-8');#convert from bytes to String
+        sensorDataFile.write(item + '\n');#add newline
+        print(item);#allow user to see what has being written to file.
     sensorDataFile.close();
+    print("Done writing data");
     
     sigHand = False
     #notify arduino that program is done writing, exit
@@ -45,21 +48,21 @@ def signal_handler(signal, frame):
     ser.close()
     os._exit(0)
 
-#write data to list
-def serPrint(ser):
-    #while user still wants data,save
+#read in data and write to list data structure
+def handleSensorData(ser):
+    #while user hasn't interrupted data collection process 
+    #save data to list
     while sigHand:
-        #makeList();
-        #print(ser.read(5));
-        strData = str(ser.read(4),'utf-8');
-        sleep(.5);
-        strData = strData.rstrip();#strip newlines
-        data.append(strData);#add to list
-        data.append("\r");#newline
-        sys.stdout.flush()
-        print(strData);
+        while ser.inWaiting(): 
+            #read in line and strip \r\n, for csv formatting purposes
+            #newline is added when written to file.
+            sData = ser.readline().strip();
+            data.append(sData);
+            print(sData);#let user see bytes, will eventually be saved to csv
+            sleep(.5);#allow time to handle data before reading again
+            sys.stdout.flush();#flush
 
-#init signal
+#init signal, waits for keyboard interrupt(Ctrl-Z)
 signal.signal(signal.SIGINT, signal_handler)
 try:
     #check num arguments
@@ -80,18 +83,18 @@ try:
     ser = serial.Serial(serial_port, 57600, timeout=0);
     sleep(2);#allow time to create serial object
     if sensor == "FLEX": 
-      initData = 'R2' + rate;
-      initData = initData.encode('utf-8');
-      ser.write(initData);
-      serPrint(ser)
+      initData = 'R2' + rate;#add rate to output data
+      initData = initData.encode('utf-8');#encode data,bytes for serial port
+      ser.write(initData);#write
+      handleSensorData(ser);#handle incoming data
     elif sensor == "FORCE":
-      initData = 'R1' + rate;
-      initData = initData.encode('utf-8');
-      ser.write(initData);
-      serPrint(ser)
+      initData = 'R1' + rate;#add rate to ouptut data
+      initData = initData.encode('utf-8');#encode data,bytes for serial port
+      ser.write(initData);#write
+      handleSensorData(ser)#handle incoming data
     else:
-        usage()
-        os._exit(1)
+        usage();#invalid input, notify user
+        os._exit(1);#exit program
 
 except:
     print( "unexpected error:", sys.exc_info());
